eedc323d1f3401a91ca9d4a833cf9fb31bd70371
bradyj04 <brj842004@gmail.com>
2021-07-04 18:10:52 -0400

    Added visible stopping indicators. Countdown only displays starting and end number.

--- main.py
+++ main.py
@@ -7,27 +7,29 @@ import time
 
 # Button Fonts/Settings
 RecordButton = sg.Button('Record', key='Record', button_color="dark red", font=("Sans Serif", 12, "bold"), size=(10, 2))
-StopButton = sg.Button('Stop', key='Stop', button_color='black', font=("Sans Serif", 12, "bold"), size=(10, 2))
 SubmitLength = sg.Button('Submit Length', key='Submit', button_color='grey', font=("Sans Serif", 10, "bold"), size=(15, 1))
 ExitButton = sg.Button('Exit', key='Exit', button_color='red', font=("Sans Serif", 12, "bold"), size=(10, 2))
+TranscriptText = sg.Multiline(size=(50,15), key='Transcript')
 
 # Defines function FileLabel, and sets text as what's defined to the right in ()
 def FileLabel(text):
     return sg.Text(text + ':', justification="center", size=(10, 1))
-#def timeelapse(newtext):
-    return sg.Text(newtext + ' seconds', key='Elapsed')
+def TimeElapsed(text):
+    return sg.Text(text, justification='left',font=("Sans Serif", 10, "bold"))
+
 # Configures layout of window
 layout = [[sg.Text("Speech-to-Text", font=('Calibri', 13, 'bold'))],
-          [sg.Text('                            ', key = 'Status', font=('Sans Serif', 10, 'italic'))],
-          #[timeelapse('0')],
+          [sg.Text('                                                                            ', key = 'Status', justification='center', font=('Sans Serif', 10, 'italic'))],
+          [TimeElapsed('Time Set: '), sg.Text('0', key='TimeRemaining',size=(5, 1), justification='center',font=("Sans Serif", 10, "bold")), TimeElapsed('seconds')],
           [FileLabel('Audio File'), sg.Input(key='InputFolder'), sg.FileBrowse(target='InputFolder')],
           [sg.Text('Recording Length:', justification='left'), sg.Input(key='AudioLengthInput', size=(30, 1), ), sg.Text('seconds', justification='left')],
           [SubmitLength],
-          [RecordButton, StopButton, ExitButton]]
+          [RecordButton, ExitButton],
+          [TranscriptText]]
 # Creates the window
 window = sg.Window('Speech-to-Text by Brady', layout, element_justification="center", finalize=True)
-
 # Display and interact with the Window
+
 while True:
     event, values = window.read()
     if event == sg.WINDOW_CLOSED or event == 'Exit':
@@ -36,6 +38,7 @@ while True:
         try:
             AFlength = int(values['AudioLengthInput'])
             window['Status'].update('Length Updated')
+            window['TimeRemaining'].update(AFlength)
         except:
             window['Status'].update('Length needed')
 
@@ -51,43 +54,45 @@ while True:
             WAVE_OUTPUT_FILENAME = "recorded.wav"
             stream = pa.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
             frames = []
-            #for timeelapsed in range (0, RECORD_SECONDS):
-                #window['Elapsed'].update(timeelapsed)
-                #time.sleep(1)
             # Updates text to match status Recording
             window['Record'].update('Recording')
             window['Status'].update('Recording...')
             # Record function goes here
-            for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+            RangeValue = RATE / CHUNK * RECORD_SECONDS
+            for recording in range(0, int(RangeValue)):
                 data = stream.read(CHUNK)
                 frames.append(data)
                 window.refresh()
                 print("1")
+            window['TimeRemaining'].update('0')
+            window['AudioLengthInput'].update('0')
+            window['Status'].update('Stopped')
+            #Changes state after time limit
+            # Updates text to match status Stop
+            window['Record'].update('Record')
+            window.refresh()
+            # Stop record function goes here
+            try:
+                # Stops recording the audio stream
+                stream.stop_stream()
+                stream.close()
+                pa.terminate()
+                wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+                wf.setnchannels(CHANNELS)
+                wf.setsampwidth(pa.get_sample_size(FORMAT))
+                wf.setframerate(RATE)
+                wf.writeframes(b''.join(frames))
+                wf.close()
+                # Gets the path to recorded file and updates the file input
+                filepath = os.path.abspath('recorded.wav')
+                window['InputFolder'].update(filepath)
+                #Updates TextBox with transcribed audio
+            except:
+                    window['Status'].update('Error, not recording')
 
     except:
         window['Status'].update('Length needed')
 
-    if event == 'Stop':
-        # Updates text to match status Stop
-        window["Record"].update('Record')
-        window['Status'].update('Stopped')
-        # Stop record function goes here
-        try:
-        #Stops recording the audio stream
-            stream.stop_stream()
-            stream.close()
-            pa.terminate()
-            wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-            wf.setnchannels(CHANNELS)
-            wf.setsampwidth(pa.get_sample_size(FORMAT))
-            wf.setframerate(RATE)
-            wf.writeframes(b''.join(frames))
-            wf.close()
-        #Gets the path to recorded file and updates the file input
-            filepath = os.path.abspath('recorded.wav')
-            window['InputFolder'].update(filepath)
-        except:
-            window['Status'].update('Error, not recording')
 
 # Closes Window
 window.close()
