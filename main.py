466f0fbd9e36604cdcf5721a7b1cc13f326bcbb5
bradyj04 <brj842004@gmail.com>
2021-07-05 01:09:27 -0400

    - Completed Base of program, audio transcription successful - Current bugs: unable to record another file unless program is restarted.

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 import os.path
+import time
+
 import PySimpleGUI as sg
 import pyaudio
 pa = pyaudio.PyAudio()
 import wave
+import speech_recognition as sr
 
 # Button Fonts/Settings
 RecordButton = sg.Button('Record', key='Record', button_color="dark red", font=("Sans Serif", 12, "bold"), size=(10, 2))
 SubmitLength = sg.Button('Submit Length', key='Submit', button_color='grey', font=("Sans Serif", 10, "bold"), size=(15, 1))
 ExitButton = sg.Button('Exit', key='Exit', button_color='red', font=("Sans Serif", 12, "bold"), size=(10, 2))
-TranscriptText = sg.Multiline(size=(50,15), key='Transcript')
-
+TranscriptText = sg.Multiline(size=(50,15), key='Transcript', justification='center')
 # Defines function FileLabel, and sets text as what's defined to the right in ()
 def FileLabel(text):
     return sg.Text(text + ':', justification="center", size=(10, 1))
 def TimeElapsed(text):
     return sg.Text(text, justification='left',font=("Sans Serif", 10, "bold"))
+#Configures speech recognition audio file
+def AudioFilePath(filepath):
+    global r
+    global audiosource
+    r = sr.Recognizer()
+    with sr.AudioFile(filepath) as source:
+        audiosource = r.record(source)
+        return r, source, audiosource
+WIT_KEY = "3LOWRYZO6UQ7JWONLAVWHN3DT7TU2ZXJ"
 
 # Configures layout of window
 layout = [[sg.Text("Speech-to-Text", font=('Calibri', 13, 'bold'))],
           [sg.Text('                                                                            ', key = 'Status', justification='center', font=('Sans Serif', 10, 'italic'))],
           [TimeElapsed('Time Set: '), sg.Text('0', key='TimeRemaining',size=(5, 1), justification='center',font=("Sans Serif", 10, "bold")), TimeElapsed('seconds')],
           [FileLabel('Audio File'), sg.Input(key='InputFolder'), sg.FileBrowse(target='InputFolder')],
-          [sg.Text('Recording Length:', justification='left'), sg.Input(key='AudioLengthInput', size=(30, 1), ), sg.Text('seconds', justification='left')],
+          [sg.Text('Recording Length:', justification='left'), sg.Input(key='AudioLengthInput', size=(30, 1), justification='left'), sg.Text('seconds', justification='left')],
           [SubmitLength],
           [RecordButton, ExitButton],
           [TranscriptText]]
@@ -86,6 +97,19 @@ while True:
                 filepath = os.path.abspath('recorded.wav')
                 window['InputFolder'].update(filepath)
                 #Updates TextBox with transcribed audio
+                AudioFilePath(filepath)
+                try:
+                    window['Status'].update('Processing...')
+                    window.refresh()
+                    TranscribedText = r.recognize_wit(audiosource, key=WIT_KEY)
+                    window.refresh()
+                    window['Transcript'].update(TranscribedText)
+                    time.sleep(1)
+                    window['Status'].update('Audio Transcribed')
+                except sr.UnknownValueError:
+                    window['Status'].update('Wit.ai could not understand')
+                except sr.RequestError as re:
+                    window['Status'].update('Request Error: {}'.format(re))
 
             except:
                     window['Status'].update('Error, not recording')
